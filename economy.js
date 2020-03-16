7256e35bcdc9034913a8b3ba373776bae752a18c
MaxundJan <62179720+MaxundJan@users.noreply.github.com>
2020-03-16 13:08:38 +0100

    Update economy.js

--- economy.js
+++ economy.js
@@ -1,6 +1,6 @@
-const Discord = require('discord.js')
-const client = new Discord.Client()
-const config = require('./config.json')
+const Discord = require('discord.js');
+const client = new Discord.Client();
+var Prefix = "€"
 
 Worker: node economy.js
 
